39b4d30914c2d54865f67e604b56f1ec4f3f7722
yeheng.ma <Myh_00@outlook.com>
2024-04-26 16:49:37 +1200

    add data model

---
+++ WebAPP/ResourceWatcher/Models/ImageModel.cs
@@ -0,0 +1,17 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ResourceWatcher.Models
+{
+    public class ImageModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime Timestamp { get; set; }
+
+        [Required]
+        [Url]
+        public string Url { get; set; }
+    }
+}
